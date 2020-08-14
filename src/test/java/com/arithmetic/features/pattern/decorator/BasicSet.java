2c091075fffac2e6b653162892f7711c1d9698c9
chenliang15405 <40684202+chenliang15405@users.noreply.github.com>
2020-08-14 11:23:01 +0800

    feat:🔥 数据结构和算法   面试必备

---
+++ src/test/java/com/arithmetic/features/pattern/decorator/BasicSet.java
@@ -0,0 +1,14 @@
+package com.arithmetic.features.pattern.decorator;
+
+/**
+ * @author alan.chen
+ * @date 2020/7/22 11:00 PM
+ */
+public class BasicSet implements Food {
+
+
+    @Override
+    public String eat() {
+        return "吃";
+    }
+}
---
+++ src/test/java/com/arithmetic/features/pattern/decorator/Food.java
@@ -0,0 +1,11 @@
+package com.arithmetic.features.pattern.decorator;
+
+/**
+ * @author alan.chen
+ * @date 2020/7/22 10:59 PM
+ */
+public interface Food {
+
+    public String eat();
+}
+
